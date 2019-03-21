bb10c879ee54025462c195e9df37b213f5d971fa
lpiquart <46375385+lpiquart@users.noreply.github.com>
2019-03-21 14:06:36 +0100

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -73,7 +73,7 @@ install_dep(){
    apt install e2fslibs # install for chattr
    apt -y install lsb-release apt-transport-https ca-certificates #install php 7.3
    wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-   echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.3.list
+   echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php7.3.list
    say_done
 }
 
